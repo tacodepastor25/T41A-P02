0d0186277683344c10b4649cd518b5abbd724729
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-09-03 16:43:33 -0600

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -55,7 +55,7 @@ def test_structure(db_connection):
                        'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
-        result_tables = {row[0] for row in cursor.fetchall()}
+        result_tables = {row[0] for row in cur.fetchall()}
         assert result_tables == expected_tables, f'''Expected tables {expected_tables},
                                                    but found {result_tables}'''
         
