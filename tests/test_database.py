a974b7683faad67b24e15ffbfdb56df71eb1428f
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-09-03 16:41:16 -0600

    Create test_database.py

---
+++ tests/test_database.py
@@ -0,0 +1,61 @@
+import psycopg2
+import pytest
+
+@pytest.fixture(scope="module")
+def db_connection():
+    conn = psycopg2.connect(
+        dbname="test_db",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
+    )
+    yield conn
+    conn.close()
+
+def test_alumnos_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM alumnos;")
+        count = cur.fetchone()[0]
+        assert count == 10
+
+def test_maestros_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM maestros;")
+        count = cur.fetchone()[0]
+        assert count == 10
+
+def test_grupos_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM grupos;")
+        count = cur.fetchone()[0]
+        assert count == 10
+
+def test_inscripciones_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM inscripciones;")
+        count = cur.fetchone()[0]
+        assert count == 10  
+
+def test_asistencia_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM asistencia;")
+        count = cur.fetchone()[0]
+        assert count == 10  
+
+def test_structure(db_connection):
+    sql = ''' 
+            SELECT tablename FROM pg_tables 
+            WHERE tablename 
+            IN ('alumnos', 'maestros', 'grupos',
+            'inscripciones', 'asistencia');
+          '''
+    expected_tables = {'alumnos', 'maestros',
+                       'grupos', 'inscripciones',
+                       'asistencia'}
+    with db_connection.cursor() as cur:
+        cur.execute(sql)
+        result_tables = {row[0] for row in cursor.fetchall()}
+        assert result_tables == expected_tables, f'''Expected tables {expected_tables},
+                                                   but found {result_tables}'''
+        
